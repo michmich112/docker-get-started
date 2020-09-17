b51c6a24ac36be01f246c5bbe8366b34c756aa55
michmich112 <cantacuzene.michel@gmail.com>
2020-09-17 07:03:40 -0400

    server implementation

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const MongoClient = require("mongodb").MongoClient;
+
+let connection = undefined;
+
+async function getConnection() {
+
+  if (connection && connection.isConnected()) return connection
+
+  const mongoUrl = process.env.MONGO_URL
+  const mongoUser = process.env.MONGO_USER
+  const mongoPass = process.env.MONGO_PASSWORD
+
+  const client = await MongoClient.connect(mongoUrl, {
+    user: mongoUser,
+    password: mongoPass,
+  });
+
+  connection = client;
+
+  return client;
+}
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function (req, res, next) {
+  try {
+    const data = await (await (await getConnection()).db('test').collection('users').find({})).toArray()
+    res.send(data)
+  } catch (e) {
+    res.send(e)
+  }
 });
+
+router.post('/add', async function (req, res, next) {
+  console.log(req.body);
+  await (await getConnection()).db('test').collection('users').replaceOne({name: req.body.name}, req.body, { upsert: true });
+  res.status(200);
+  res.end();
+})
 
 module.exports = router;
---
+++ server/start.sh
@@ -0,0 +1,4 @@
+MONGO_URL="mongodb://localhost:27017/" \
+MONGO_USER=root \
+MONGO_PASSWORD=password \
+npm start
